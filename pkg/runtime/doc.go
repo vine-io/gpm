a4c692623218c37db5b6b57deaf747481cc10b0b
lack <598223084@qq.com>
2021-08-06 12:06:53 +0800

    v1.1.1

--- pkg/runtime/doc.go
+++ pkg/runtime/doc.go
@@ -29,9 +29,9 @@ const GpmName = "go.vine.service.gpm"
 const GpmId = "12df4108-6176-4c93-ac58-bfa003d4ddb1"
 
 var (
-	GitTag     = "v1.1.0"
-	GitCommit  = "0ba7c82"
-	BuildDate  = "1628222515"
+	GitTag     = "v1.1.1"
+	GitCommit  = "f195f26"
+	BuildDate  = "1628222806"
 	GetVersion = func() string {
 		v := GitTag
 		if GitCommit != "" {
