3f12dd932059058dd8f8d4c3addca5e27a0931e0
lack-io <598223084@qq.com>
2023-02-24 17:10:52 +0800

    v1.10.2

--- pkg/internal/doc.go
+++ pkg/internal/doc.go
@@ -29,9 +29,9 @@ const GpmName = "io.gpm.service.gpm"
 const GpmId = "12df4108-6176-4c93-ac58-bfa003d4ddb1"
 
 var (
-	GitTag     = "v1.10.1"
-	GitCommit  = "189c18a"
-	BuildDate  = "1675818434"
+	GitTag     = "v1.10.2"
+	GitCommit  = "d9eeb18"
+	BuildDate  = "1677229852"
 	GetVersion = func() string {
 		v := GitTag
 		if GitCommit != "" {
